915dea7671549c8683b07abfb79252fa08c1d0b3
Benjamin Frisanco <frisanco@cse.msu.edu>
2019-04-02 14:12:55 -0400

    Convert to php; create view class

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
+<?php
+$view = new Site\View();
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
-    <link href="/normalize.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
-    integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <meta charset="utf-8">
-    <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash|Lobster|Pacifico|Patua+One|Orbitron" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="page.css">
-    <link rel="shortcut icon" href="spartan-helmet-og.ico" type="image/x-icon" />
-    <title> Ben Frisanco </title>
+    <?php echo $view->header(); ?>
   </head>
   <body>
 
